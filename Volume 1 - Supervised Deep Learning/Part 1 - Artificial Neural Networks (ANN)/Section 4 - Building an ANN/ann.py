e1464c34075e862090f4c35e023d666467e00b2a
pmtempone <pmtempone@gmail.com>
2018-07-29 19:44:35 -0300

    Ejercicio sección 5 finalizado

--- Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
@@ -75,3 +75,27 @@ y_pred = (y_pred > 0.5)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+cm
+
+X_test[1,:]
+
+# Ejemplo
+
+# Geography: France
+# Credit Score: 600
+# Gender: Male
+# Age: 40 years old
+# Tenure: 3 years
+# Balance: $60000
+# Number of Products: 2
+# Does this customer have a credit card ? Yes
+# Is this customer an Active Member: Yes
+# Estimated Salary: $50000
+
+X_test_ejercicio = np.array([[0,0,600,1,40,3,60000,2,1,1,50000]])
+
+X_test_ejercicio = sc.transform(X_test_ejercicio)
+
+y_pred_ejercicio = classifier.predict(X_test_ejercicio)
+y_pred_ejercicio = (y_pred_ejercicio > 0.5)
