faa358e2c604b1a9a87e7646c85cc211e6ba38f3
pmtempone <pmtempone@gmail.com>
2018-07-31 22:00:39 -0300

    Ejercicio sección 6 finalizado

--- Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
@@ -99,3 +99,63 @@ X_test_ejercicio = sc.transform(X_test_ejercicio)
 
 y_pred_ejercicio = classifier.predict(X_test_ejercicio)
 y_pred_ejercicio = (y_pred_ejercicio > 0.5)
+
+# Seccion 6 parte 1
+
+# Evaluating the ANN
+
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from keras.models import Sequential
+from keras.layers import Dense
+
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(output_dim = 6, init = 'uniform', activation='relu',input_dim=11))
+    classifier.add(Dense(output_dim=6, init='uniform', activation='relu'))
+    classifier.add(Dense(output_dim=1, init='uniform', activation='sigmoid'))
+    classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return classifier
+
+classifier = KerasClassifier(build_classifier, batch_size=10,nb_epoch=100)
+accuracies = cross_val_score(estimator=classifier,X=X_train,y=y_train,cv=10,n_jobs=-1)
+variance = accuracies.std()
+
+# Seccion 6 parte 2
+
+# Tunning the ANN
+
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+
+def build_classifier(optimizer):
+    classifier = Sequential()
+    classifier.add(Dense(output_dim = 6, init = 'uniform', activation='relu',input_dim=11))
+    classifier.add(Dense(output_dim=6, init='uniform', activation='relu'))
+    classifier.add(Dense(output_dim=1, init='uniform', activation='sigmoid'))
+    classifier.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    return classifier
+
+
+classifier = KerasClassifier(build_fn=build_classifier)
+
+parameters = {'batch_size': [25,32],
+              'nb_epoch': [100,500],
+              'optimizer': ['adam','rmsprop']}
+
+grid_search = GridSearchCV(estimator=classifier,
+                           param_grid=parameters,
+                           scoring='accuracy',
+                           cv=10)
+
+grid_search = grid_search.fit(X_train,y_train)
+
+best_parameters = grid_search.best_params_
+
+best_accuracy = grid_search.best_score_
+
+best_parameters
+
+best_accuracy
